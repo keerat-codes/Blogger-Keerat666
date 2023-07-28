eec0fa3603db4a639f766458396145a5270ad3fc
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-29 01:22:53 +0530

    Updated App.js for Blog route using AutoPilot

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 8009;
 
 require('dotenv').config();
 
+const BlogRoute = require('./routes/Blog');
+app.use('/Blog', BlogRoute);
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
